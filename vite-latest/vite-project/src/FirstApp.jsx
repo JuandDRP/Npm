09aaa342246a456fc6c820c0f4f3badd720dfc0a
JuandDRP <juandrp2004@gmail.com>
2024-04-03 16:32:36 -0500

    a

--- vite-latest/vite-project/src/FirstApp.jsx
+++ vite-latest/vite-project/src/FirstApp.jsx
@@ -1,24 +1,53 @@
 import PropTypes from 'prop-types'
+import { useState } from 'react'
 
-const FirstApp=({title,sum})=>{
+const FirstApp=({value})=>{
+    const [counter,setCounter]=useState(value)
+
+    const handleAdd=()=>{
+        setCounter(counter + 1)
+    }
+
+    const handleSubstract=()=>{
+        setCounter(counter-1)
+    }
+
+    const handleReset=()=>{
+        setCounter(0)
+    }
+
     return (
-        
         <>
-        <h1>{title}</h1>
-        <span>{sum}</span>
+        <h1>counter</h1>
+        <span>{counter}</span>
+        <button onClick={()=>handleAdd()}>+1</button>
+        <button onClick={()=>handleSubstract()}>-1</button>
+        <button onClick={()=>handleReset()}>Reset</button>
         </>   
     )
 }
 
 FirstApp.propTypes={
     title:PropTypes.string.isRequired,
-    sum:PropTypes.number.isRequired
+    value:PropTypes.number.isRequired
 }
 
 FirstApp.defaultProps={
     title:'No hay titulo',
-    sum:300
+    value:0
 }
+
+// const FirstApp=({title,sum})=>{
+//     return (
+        
+//         <>
+//         <h1>{title}</h1>
+//         <span>{sum}</span>
+//         </>   
+//     )
+// }
+
+
 
 
 
